b18e155ca2fa8c242655b00deff72a5f5c891240
BukanWibi <138184838+BukanWibi@users.noreply.github.com>
2023-07-03 19:44:02 +0700

    Update Coba.py

--- Coba.py
+++ Coba.py
@@ -2,4 +2,4 @@ import streamlit as s
 s.title("Ini Aku!")
 from PIL import Image
 image = Image.open('banner.png')
-st.image(image, caption='Sunrise by the mountains')
+s.image(image, caption='Sunrise by the mountains')
