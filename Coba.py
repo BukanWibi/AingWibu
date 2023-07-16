52d35a17d121f7cd518a1b4dec6fb6c917d2e92d
BukanWibi <138184838+BukanWibi@users.noreply.github.com>
2023-07-16 11:54:24 +0700

    Delete Coba.py

--- Coba.py
+++
@@ -1,7 +0,0 @@
-import streamlit as s
-from PIL import Image
-image = Image.open('banner.png')
-s.image(image, caption=' ')
-s.title("Pengambil Warna")
-color = s.color_picker('Pilih Warna Kesukaanmu', '#00f900')
-s.write('Kode Warna nya: ', color)
